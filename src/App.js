d07fc07ea775a6bc275857a1780d74b1e73ac17d
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-05 16:50:23 +0500

    Implementing Suspenese and Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
+
+const Data = lazy(() => import("./Data"));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Data />
+      </Suspense>
     </div>
   );
 }
